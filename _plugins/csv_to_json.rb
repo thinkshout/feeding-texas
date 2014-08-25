5b6dc3bfa518c386fd8ef4bcf54aae55c2fb6d5b
Lev Tsypin <lev@thinkshout.com>
2014-08-25 15:42:46 -0700

    Consolidating custom plugins.

--- _plugins/csv_to_json.rb
+++ _plugins/csv_to_json.rb
@@ -25,6 +25,37 @@ module Jekyll
     end
 
   end
+
+  class FoodbankPage < Page
+    def initialize(site, base, dir, location)
+      @site = site
+      @base = base
+      @dir = dir
+      @name = "#{location['permalink']}/"
+
+      self.process(@name)
+      self.read_yaml(File.join(base, '_layouts'), 'location.html')
+      self.data['url'] = "#{dir}/#{location['permalink']}/"
+
+      self.data['title'] = "#{location['title']}"
+      self.data['location'] = location
+    end
+  end
+
+  class FoodbankGenerator < Jekyll::Generator
+    safe true
+
+    def generate(site)
+
+      dir = site.config['location_dir'] || 'locations'
+      site.data['foodbanks'].each do |location|
+        site.pages << FoodbankPage.new(site, site.source, dir, location)
+      end
+
+    end
+
+  end
+
 end
 
 Liquid::Template.register_tag('CSVZipToOptions', Jekyll::CSVZipToOptions)
