0684d849444ce6faf37dcb5220655a8cdd61b222
Lev Tsypin <lev@thinkshout.com>
2014-08-26 13:02:28 -0700

    Deleting unused Grunt files and moving Compass config file to project root.

---
+++ config.rb
@@ -0,0 +1,12 @@
+# Require any additional compass plugins here.
+# add_import_path "bower_components/foundation/scss"
+require "susy"
+require 'breakpoint'
+require 'sass-globbing'
+
+# Set this to the root of your project when deployed:
+http_path = "assets/"
+css_dir = "assets/stylesheets"
+sass_dir = "assets/sass"
+images_dir = "assets/images"
+javascripts_dir = "assets/js"
