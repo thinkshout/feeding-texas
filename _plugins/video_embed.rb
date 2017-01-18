ed6c09c590e105b74d226e2076439e6f1eda025e
Eric Paxton <heypaxton@gmail.com>
2017-01-18 13:19:09 -0800

    Set video and map embed to handle https

--- _plugins/video_embed.rb
+++ _plugins/video_embed.rb
@@ -24,13 +24,13 @@ class Embed < Liquid::Tag
   def render(context)
     case @service
       when "youtube"
-        "<iframe frameborder=\"0\" width=\"#{@width}\" height=\"#{@height}\" src=\"http:\/\/www.youtube.com\/embed/#{@id}?color=white&theme=light\"><\/iframe>"
+        "<iframe frameborder=\"0\" width=\"#{@width}\" height=\"#{@height}\" src=\"\/\/www.youtube.com\/embed/#{@id}?color=white&theme=light\"><\/iframe>"
       when "facebook"
-        "<iframe src=\"http:\/\/www.facebook.com/video/embed?video_id=#{@id}\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\"><\/iframe>"
+        "<iframe src=\"\/\/www.facebook.com/video/embed?video_id=#{@id}\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\"><\/iframe>"
       when "vimeo"
         "<iframe src=\"\/\/player.vimeo.com\/video\/#{@id}?badge=0\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" webkitallowfullscreen mozallowfullscreen allowfullscreen><\/iframe>"
       when "map"
-        "<iframe src=\"https:\/\/www.google.com\/maps\/d\/embed?#{@id}\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" style=\"border:0\" allowfullscreen><\/iframe>"
+        "<iframe src=\"\/\/www.google.com\/maps\/d\/embed?#{@id}\" width=\"#{@width}\" height=\"#{@height}\" frameborder=\"0\" style=\"border:0\" allowfullscreen><\/iframe>"
     end
   end
 
